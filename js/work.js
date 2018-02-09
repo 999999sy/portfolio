2be5d5e3f1143b80feebf446d136b36954e3b1d6
999999sy <33650909+999999sy@users.noreply.github.com>
2018-02-09 19:19:17 +0900

    dddd

--- js/work.js
+++ js/work.js
@@ -1,8 +1,53 @@
 $(document).ready(function(){
 
-  // slide
+  slider();
 
-  
 
 
 })
+
+function slider(){
+  // slider
+  var li_width = $('ul.work_list li').width();
+  var list_length = $('ul.work_list li').length;
+  console.log(li_width);
+  console.log(list_length);
+
+  var list = $('ul.work_list');
+  var list_first = $('ul.work_list li:first-child').clone();
+  var list_last = $('ul.work_list li:last-child').clone();
+
+  list.append(list_first);
+  list.prepend(list_last);
+
+  // for(i=0; i<list_length; i++){
+  //  $('.work_slider ul.work_list').animate({'marginLeft':-li_width*i},2000);
+  // }
+
+  // slide button
+ var num = 0;
+
+
+  $('.slide_button .button_right').on('click',function(){
+    num--;
+    $('ul.work_list').stop().animate({'marginLeft':li_width*num},2000);
+      if(-list_length==num-1){
+        num=1;
+        list.css('margin-left','num')
+      }
+      console.log(num);
+      console.log(-list_length);
+  })
+
+  $('.slide_button .button_left').on('click',function(){
+    num++;
+    $('ul.work_list').stop().animate({'marginLeft':li_width*num},2000);
+    if(list_length==num+1){
+      num=0;
+      list.css('margin-left','li_width');
+    }
+
+    console.log(num);
+    console.log(list_length);
+  })
+}
