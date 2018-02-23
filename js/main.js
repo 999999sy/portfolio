fe82c3755401d06028ba1085736ad2719e84b203
999999sy <33650909+999999sy@users.noreply.github.com>
2018-02-23 16:11:56 +0900

    dddd

--- js/main.js
+++ js/main.js
@@ -1,43 +1,46 @@
-$(window).resize(function(){
+$(window).resize(function() {
   window_h();
 })
 
 
-$(document).ready(function(){
+$(document).ready(function() {
+
+  // scroll paging add
+  $(document).on("scroll", onScroll);
 
   //window height
   window_h();
-haha_img();
-  // window scroll
-  $(window).scroll(function(){
-      var nav_height = $('nav').height();
-      var imgsize =$(this).scrollTop();
-      var top = $('header').offset().top;
-  if(imgsize>2){
+  haha_img();
+
+  // scroll nav opacity
+  $(window).scroll(function() {
+    //  nav scroll opacity
+    var nav_height = $('nav').height();
+    var imgsize = $(this).scrollTop();
+    var top = $('header').offset().top;
+    if (imgsize > 2) {
       $('nav').css({
-          border:'0px',
-          position:'fixed',
-          top:'0',
-          opacity:'0.9',
+        border: '0px',
+        position: 'fixed',
+        top: '0',
+        opacity: '0.9',
 
       });
-       }
-      else {
-          $('nav').css("position","relative")
-      }
+    } else {
+      $('nav').css("position", "relative")
+    }
 
+  })
 
-   });
-
-   // page scroll
+  // nav click paging
   $('nav ul li a.color_hold').addClass('color');
-  $('nav ul li a').on('click',function(event){
+  $('nav ul li a').on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top,
-      }, 900, function(){
+      }, 900, function() {
         window.location.hash = hash;
       });
     } // End if
@@ -48,14 +51,14 @@ haha_img();
 
   });
 
-    // main text button click pagination
-  $('.main_text span.button a').on('click',function(event){
+  // main text button (start) click pagination
+  $('.main_text span.button a').on('click', function(event) {
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top,
-      }, 900, function(){
+      }, 900, function() {
         window.location.hash = hash;
       });
     } // End if
@@ -69,16 +72,41 @@ haha_img();
 })
 
 // main img opacity
-function haha_img(){
-  $('.haha img').animate({opacity:0},'slow');
-  $('.haha img').animate({opacity:1},'slow');
+function haha_img() {
+  $('.haha img').animate({
+    opacity: 0
+  }, 'slow');
+  $('.haha img').animate({
+    opacity: 1
+  }, 'slow');
 }
 
-// height 값 조정
-function window_h(){
+// window height 값 조정
+function window_h() {
   // window 맞게 height 값 조정
-  var nav_height =$('nav').height();
-  var window_height = $(window).height()-nav_height;
+  var nav_height = $('nav').height();
+  var window_height = $(window).height() - nav_height;
 
-  $('.laugh').css({'height':window_height});
+  $('.laugh').css({
+    'height': window_height
+  });
+}
+
+
+// scroll paging //codepen.io 참고 제이쿼리
+function onScroll(event) {
+  var scrollPos = $(document).scrollTop() + 300;
+  $('nav ul li a').each(function() {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('nav ul li a').removeClass("color");
+      currLink.addClass("color");
+      var pageURL = $(location).attr("href");
+      var splittedURL = pageURL.split('/')[0];
+      //alert(splittedURL);
+    } else {
+      currLink.removeClass("color");
+    }
+  });
 }
--- js/project.js
+++ js/project.js
@@ -23,30 +23,23 @@ function slider(){
   var list_first = $('ul.work_list li:first-child').clone();
   var list_last = $('ul.work_list li:last-child').clone();
 
-  list.append(list_first);
-  list.prepend(list_last);
-
-  // for(i=0; i<list_length; i++){
-  //  $('.work_slider ul.work_list').animate({'marginLeft':-li_width*i},2000);
-  // }
 
   // slide button
- var num = -1;
+ var num = 0;
 
 
   $('.slide_button .button_right').on('click',function(){
     num--;
     $('ul.work_list').stop().animate({'marginLeft':li_width*num},1000);
-      if(-list_length==num-1){
+      if(-list_length==num-3){
         num=1;
         list.css('margin-left','num');
       }
       console.log(num);
       console.log(-list_length);
   })
-
+  num=0;
   $('.slide_button .button_left').on('click',function(){
-    num=0;
     num++;
     $('ul.work_list').stop().animate({'marginLeft':li_width*num-400},1000);
     if(list_length==num){
